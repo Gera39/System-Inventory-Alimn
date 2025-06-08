f231cdad04573dda7dbad41c31fc5832afdf1fb9
Gera39 <jerrymoraleslara39@gmail.com>
2025-06-07 21:23:48 -0600

    Subo contenido en archivos

--- src/views/PanelDashboardView.vue
+++ src/views/PanelDashboardView.vue
@@ -1,13 +1,13 @@
 <template>
     <SidebarPage/>
-    <div class="p-4 sm:ml-64">
-    HOLISSSS
+     <div class="p-4 sm:ml-64">
+        <Router-View/>
     </div>
+   
 </template>
 
 <script lang="ts" setup>  
-import SidebarPage from '../pages/SidebarPage.vue';
+import SidebarPage from '../layout/SidebarPage.vue';
+// import InventoryPage from '../pages/InventoryPage.vue';
 </script>
-
-
 <style scoped></style>
